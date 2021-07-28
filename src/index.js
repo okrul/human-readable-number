71a4f3a2c962200971475da1642e0672102e4c3a
Okrul <okrul@list.ru>
2021-07-28 19:45:40 +0300

    feat: implement function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
+    let a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    let re = '';
+
+    if (number == 0) {
+        r = 'zero'
+        return r;
+    }
+    else if (number < 20) {
+        r = a[number];
+        return r;
+    }
+    else if (number >= 20 && number <100) {
+        r = b[Math.trunc(number/10)];
+        if (number%10 > 0) {
+           r += ' ' + toReadable(number%10); 
+        } 
+    }
+    else if (number >= 100) {
+        r = a[Math.trunc(number/100)] + ' hundred';
+        if  (number%100 > 0) {
+            r += ' ' + toReadable(number%100); 
+        }
+    }
+
+    return r;
+     
 }
